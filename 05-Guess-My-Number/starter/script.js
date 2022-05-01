989fa55c3c41bb360e7018426c12f372c8d9afc1
99SOHI <alwiprtm66@gmail.com>
2022-05-02 01:41:05 +0700

    50%? progress

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', () => {
+    let guessValue = Number(document.querySelector('.guess').value);
+
+
+
+    if (!guessValue) {
+        document.querySelector('.message').textContent = "Isi yang bener kocak"
+    } else if (guessValue > 20) {
+        document.querySelector('.message').textContent = "Cuma nyampe 20 kocak, kalo lebih kesusahan lunyaa"
+    } else if (guessValue < 1) {
+        document.querySelector('.message').textContent = "Mau ngapain lu ngisi angka negatif, hdehhh sok asik"
+    } else if (guessValue === secretNumber) {
+        document.querySelector('.message').textContent = "Anjay Bener luhh"
+        document.querySelector(".score").textContent = score;
+    } else if (guessValue > secretNumber) {
+        document.querySelector('.message').textContent = "Ketinggian";
+        score--;
+        document.querySelector(".score").textContent = score;
+    } else if (guessValue < secretNumber) {
+        document.querySelector('.message').textContent = "Kekecilan";
+        score--;
+        document.querySelector(".score").textContent = score;
+    }
+})
